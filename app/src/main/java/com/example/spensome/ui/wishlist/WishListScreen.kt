3eb1ce00f36484a5763bea820503cc1453d35ffe
omolchanova <olga.molchanova@bia-tech.ru>
2023-09-06 15:12:29 +0300

    migrate to material3

--- app/src/main/java/com/example/spensome/ui/wishlist/WishListScreen.kt
+++ app/src/main/java/com/example/spensome/ui/wishlist/WishListScreen.kt
@@ -2,19 +2,21 @@ package com.example.spensome.ui.wishlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,74 +40,69 @@ fun WishListScreen(
     modifier: Modifier = Modifier,
     products: List<Product>
 ) {
-    SpensomeTheme {
-        Column(
-            modifier = modifier
-                .background(color = Color.White)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center
-        ) {
-            if (products.isEmpty()) {
-                Text(
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    text = "Wish list is empty!",
-                    color = Color.Black.copy(alpha = 0.5f),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp
-                )
-            } else {
-                WishList(
-                    products = ProductsRepository.products
-                )
-            }
+    Column(
+        modifier = modifier
+            .background(color = Color.White)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (products.isEmpty()) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = "Wish list is empty!",
+                color = Color.Black.copy(alpha = 0.5f),
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        } else {
+            WishList(
+                products = ProductsRepository.products
+            )
         }
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun WishListItem(
     modifier: Modifier = Modifier,
     product: Product
 ) {
-    SpensomeTheme {
-        Card(
-            modifier = modifier,
-            elevation = 4.dp,
-            backgroundColor = MaterialTheme.colors.secondary,
-            onClick = {
-                // TODO
-            }
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        modifier = modifier.clickable {
+            // TODO
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(
-                        id = product.imageRes ?: R.drawable.ic_launcher_foreground
-                    ),
-                    contentDescription = "Product item image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .clip(shape = MaterialTheme.shapes.medium)
-                        .background(color = MaterialTheme.colors.secondary)
+            Image(
+                painter = painterResource(
+                    id = product.imageRes ?: R.drawable.ic_launcher_foreground
+                ),
+                contentDescription = "Product item image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .background(color = MaterialTheme.colorScheme.secondary)
+            )
+            Text(
+                text = product.title,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(
+                    top = dimensionResource(id = R.dimen.padding_small)
                 )
-                Text(
-                    text = product.title,
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.h6,
-                    modifier = Modifier.padding(
-                        top = dimensionResource(id = R.dimen.padding_small)
-                    )
-                )
-                Text(
-                    text = "${product.price} $",
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.subtitle1
-                )
-            }
+            )
+            Text(
+                text = "${product.price} $",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.labelMedium
+            )
         }
     }
 }
@@ -140,24 +137,30 @@ fun WishList(
 @Preview(heightDp = 720, widthDp = 360)
 @Composable
 private fun WishListScreenPreview() {
-    WishListScreen(products = ProductsRepository.products)
+    SpensomeTheme {
+        WishListScreen(products = ProductsRepository.products)
+    }
 }
 
 @Preview
 @Composable
 private fun WishListItemPreview() {
-    WishListItem(
-        product = Product(
-            title = "Test",
-            price = 105f
+    SpensomeTheme {
+        WishListItem(
+            product = Product(
+                title = "Test",
+                price = 105f
+            )
         )
-    )
+    }
 }
 
 @Preview
 @Composable
 private fun WishListPreview() {
-    WishList(
-        products = ProductsRepository.products
-    )
+    SpensomeTheme {
+        WishList(
+            products = ProductsRepository.products
+        )
+    }
 }
